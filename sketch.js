41ecdec131fbb49a95cc0c6545e20264815d0c62
ojas-vinze <71540421+ojas-vinze@users.noreply.github.com>
2020-12-17 21:55:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Mouse=Matter.Mouse;
 const MouseConstraint=Matter.MouseConstraint;
 
 function setup(){
-    canvas= createCanvas(windowWidth/2,windowHeight/1.5);
+    canvas= createCanvas(1200,570);
     engine=Engine.create();
     world=engine.world;
 
@@ -18,14 +18,22 @@ function setup(){
     mconstraint=MouseConstraint.create(engine,options);
     World.add(world,mconstraint);
 
-    pendulum1 = new Pendulum(width-30,height-40,200);
-    pendulum2 = new Pendulum(width-50,height-40,200);
-    pendulum3 = new Pendulum(width-70,height-40,200);
-    pendulum4 = new Pendulum(width-90,height-40,200);
-    pendulum5 = new Pendulum(width-120,height-40,200);
+    pendulum1 = new Pendulum(220,100,0);
+    pendulum2 = new Pendulum(260,100,0);
+    pendulum3 = new Pendulum(300,100,0);
+    pendulum4 = new Pendulum(340,100,0);
+    pendulum5 = new Pendulum(380,100,0);
+
+    sling1 = new Sling(pendulum1.body,{x:220,y:60});
+    sling2 = new Sling(pendulum2.body,{x:260,y:60});
+    sling3 = new Sling(pendulum3.body,{x:300,y:60});
+    sling4 = new Sling(pendulum4.body,{x:340,y:60});
+    sling5 = new Sling(pendulum5.body,{x:380,y:60});
 }
 
 function draw(){
+
+    background("lightgrey")
 
     pendulum1.display();
     pendulum2.display();
@@ -33,9 +41,15 @@ function draw(){
     pendulum4.display();
     pendulum5.display();
 
+    sling1.display();
+    sling2.display();
+    sling3.display();
+    sling4.display();
+    sling5.display();
+
     Engine.update(engine);
 }
 
 function mouseDragged(){
-    Matter.Body.setposition(pendulum1.body,{x:mouseX, y:mouseY});
+    Matter.Body.setPosition(pendulum1.body,{x:mouseX, y:mouseY});
 }
